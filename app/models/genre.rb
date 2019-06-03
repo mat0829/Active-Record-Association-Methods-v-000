c0e40a01af3800ae15c0ba7d13335a48d1c506eb
David Anderson <matandersonlv@hotmail.com>
2019-06-03 17:24:57 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.all
+    self.artists.where(:name)
   end
 end
